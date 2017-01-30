f2839756013c16fe6c85b84d4e55c9ea14f85749
rogerk <rog_kol03@hotmail.com>
2017-01-30 23:00:30 +0100

    testing

--- view/Overview.php
+++ view/Overview.php
@@ -1,14 +1,26 @@
    
 <?php
 $results = $GLOBALS["dummy"];
-
 ?>
 
 <div id="main-wrapper">
-      <?php foreach($results as $results): ?>        
-        <?php echo "ID: " . $results['ID'] . ",  Brukernavn: " .$results['brukernavn'] . ",  Info: " .$results['tekst'];  ?><br>
+    <?php foreach ($results as $results): ?>        
+        <?php echo "ID: " . $results['ID'] . ",  Brukernavn: " . $results['brukernavn'] . ",  Info: " . $results['tekst']; ?><br>
 
 
     <?php endforeach; ?>
-    </div>
-    
+
+
+    <br> <br>
+    <b>Opprett ny brukerting</b>
+
+    <form action="?page=addDummy" method="post">
+        ID:<br>
+        <input type="int" name="ID" value=""><br>
+        Brukernavn:<br>
+        <input type="text" name="brukernavn" value=""><br>
+        Info:<br>
+        <input type="text" name="tekst" value=""><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
